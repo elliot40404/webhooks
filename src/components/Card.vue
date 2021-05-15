78c52e720cd266ee4a41fa6b2c4fc0bc6724c0dd
elliot404 <fortesting40404@gmail.com>
2021-05-15 17:10:19 +0530

    cards

---
+++ src/components/Card.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+      <h1>CARD</h1>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Card'
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Api.vue
+++ src/views/Api.vue
@@ -1,15 +1,24 @@
 <template>
   <div>
-      <h1>⚡ API PAGE</h1>
+    <h1>⚡ API PAGE</h1>
+    <div class="grid">
+      <Card/>
+    </div>
   </div>
 </template>
 
 <script>
+import Card from '@/components/Card.vue'
 export default {
-    name: 'Api'
+    name: 'Api',
+    components: {
+      Card
+    }
 }
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+h1 {
+  padding: 1em;
+}
 </style>
